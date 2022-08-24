9a9e590d4922427d47f0268285bff2be560bb501
anna <annabarellaalonso@gmail.com>
2022-08-24 22:42:36 +0200

    Template and dynamic names

--- java.js
+++ java.js
@@ -1,14 +1,35 @@
 console.log("hej");
 
 fetch("./actors.json")
-  .then(function (resp) {
+  .then((resp) => {
     return resp.json();
   })
-
-  .then(function (data) {
-    console.log(data);
+  .then((data) => {
+    actorCinema(data);
   });
 
+function actorCinema(data) {
+  console.log(data);
+  data.forEach(showInfo);
+  /*  const template = document.querySelector("template");
+  const clone = template.cloneNode(true);
+
+  clone.querySelector("fullname").textContent = information.fullname;
+
+  document.querySelector("main").appendChild(clone); */
+}
+
+function showInfo(info) {
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+
+  clone.querySelector(".fullname").textContent = info.fullname;
+
+  const parent = document.querySelector("main");
+  parent.appendChild(clone);
+}
+
+/* Modal */
 const modal = document.querySelector(".modal");
 const openModal = document.querySelectorAll(".jopen");
 const closeModal = document.querySelector(".jclose");
@@ -16,7 +37,6 @@ const overlay = document.getElementById("overlay");
 
 openModal.forEach((button) => {
   button.addEventListener("click", () => {
-    /* console.log("open"); */
     modal.showModal();
     openOverlay(overlay);
   });
@@ -36,7 +56,7 @@ function openOverlay(overlay) {
 }); */
 
 closeModal.addEventListener("click", () => {
-  /* console.log("closed"); */
+  /*   console.log("closed"); */
   modal.close();
   closeOverlay(overlay);
 });
