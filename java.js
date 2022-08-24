7d334148d48b2be2cf219da16b38d218df5e0171
anna <annabarellaalonso@gmail.com>
2022-08-24 19:05:46 +0200

    Js to open and close modal

---
+++ java.js
@@ -0,0 +1,38 @@
+console.log("hej");
+
+const modal = document.querySelector(".modal");
+const openModal = document.querySelectorAll(".jopen");
+const closeModal = document.querySelector(".jclose");
+const overlay = document.getElementById("overlay");
+
+openModal.forEach((button) => {
+  button.addEventListener("click", () => {
+    /* console.log("open"); */
+    modal.showModal();
+    openOverlay(overlay);
+  });
+});
+
+function openOverlay(overlay) {
+  if (overlay == null) return;
+  overlay.classList.add("active");
+}
+
+/* closeModal.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("closed");
+    modal.close();
+    closeOverlay(overlay);
+  });
+}); */
+
+closeModal.addEventListener("click", () => {
+  /* console.log("closed"); */
+  modal.close();
+  closeOverlay(overlay);
+});
+
+function closeOverlay(overlay) {
+  if (overlay == null) return;
+  overlay.classList.remove("active");
+}
