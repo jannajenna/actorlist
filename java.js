abbfcb6148780a59a37432b6c70a1c9e18aea279
anna <annabarellaalonso@gmail.com>
2022-08-28 23:02:10 +0200

    working soution

--- java.js
+++ java.js
@@ -17,57 +17,27 @@ function showInfo(info) {
   const template1 = document.querySelector(".template1").content;
   const clone = template1.cloneNode(true);
 
-  clone.querySelector(".fullname").textContent = info.fullname;
+  clone.querySelector(".fullname1").textContent = info.fullname;
+
+  clone.querySelector(".jopen").addEventListener("click", showModal);
+
+  function showModal() {
+    console.log("opened");
+    document.querySelector(".modal").classList.add("active");
+    document.querySelector(".overlay").classList.remove("activeo");
+
+    document.querySelector(".fullname2").textContent = `${info.fullname}`;
+    document.querySelector(".movie").textContent = `${info.movie}`;
+
+    document.querySelector(".jclose").addEventListener("click", hideModal);
+
+    function hideModal() {
+      console.log("closed");
+      document.querySelector(".modal").classList.remove("active");
+      document.querySelector(".overlay").classList.add("activeo");
+    }
+  }
 
   const parent = document.querySelector("main");
   parent.appendChild(clone);
 }
-
-/* Modal */
-
-const modal = document.querySelectorAll(".modal");
-const openModal = document.querySelectorAll(".jopen");
-const closeModal = document.querySelectorAll(".jclose");
-const overlay = document.querySelectorAll("overlay");
-
-openModal.addEventListener("click", function () {
-  modal.style.display = "block";
-  overlay.style.display = "inherit";
-});
-
-/* const modal = document.querySelector(".modal");
-const openModal = document.querySelectorAll(".jopen");
-const closeModal = document.querySelector(".jclose");
-const overlay = document.getElementById("overlay");
-
-openModal.forEach((button) => {
-  button.addEventListener("click", () => {
-    modal.showModal();
-    openOverlay(overlay);
-  });
-});
-
-function openOverlay(overlay) {
-  if (overlay == null) return;
-  overlay.classList.add("active");
-}
-
-closeModal.forEach((button) => {
-  button.addEventListener("click", () => {
-    console.log("closed");
-    modal.close();
-    closeOverlay(overlay);
-  });
-});
-
-closeModal.addEventListener("click", () => {
-    console.log("closed");
-  modal.close();
-  closeOverlay(overlay);
-});
-
-function closeOverlay(overlay) {
-  if (overlay == null) return;
-  overlay.classList.remove("active");
-}
- */
